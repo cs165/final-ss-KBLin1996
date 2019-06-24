12d4854f29eabdb6e6661259eba8245484747d32
KB <popotwtw@gmail.com>
2019-06-24 18:11:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const googleSheets = require('gsa-sheets');
 
 // TODO(you): Update the contents of privateSettings accordingly, as you did
 // in HW5, then uncomment this line.
-// const key = require('./privateSettings.json');
+const key = require('./privateSettings.json');
 
 // TODO(you): Change the value of this string to the spreadsheet id for your
 // GSA spreadsheet, as you did in HW5, then uncomment these lines.
-// const SPREADSHEET_ID = '__YOUR__SPREADSHEET__ID__HERE__';
-// const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
+const SPREADSHEET_ID = '1qS1x1y0PkhylqsARO0jzLRgM6qAptZz1B9cY8esQ_WA';
+const sheet = googleSheets(key.client_email, key.private_key, SPREADSHEET_ID);
 
 const app = express();
 const jsonParser = bodyParser.json();
@@ -17,6 +17,43 @@ const jsonParser = bodyParser.json();
 app.use(express.static('public'));
 
 // TODO(you): Add at least 1 GET route and 1 POST route.
+async function onGet(req, res) {
+	const month=req.params.month
+	const day=req.params.date
+	const result = await sheet.getRows();
+	const rows = result.rows;
+	console.log(rows);
+	for(i=0;i<rows.length;i++){
+		if(rows[i][0] === month+'/'+day)
+		{
+			res.json({'res':rows[i][1]})
+			return
+		}
+	}
+	res.json({'stat':'succ'})
+}
+app.get('/:month/:date', onGet)
+
+async function onPost(req, res) {
+	const month=req.params.month
+	const day=req.params.date
+	const messageBody = req.body;
+
+	const result = await sheet.getRows();
+	const rows = result.rows;
+	set=false
+	var i
+	for(i=0;i<rows.length;i++){
+		if(rows[i][0] === month+'/'+day)
+		{
+			await sheet.setRow(i,[month+'/'+day,messageBody.text])
+			set=true
+		}
+	}
+	if(set===false)
+		await sheet.setRow(i,[month+'/'+day,messageBody.text])
+}
+app.post('/:month/:date', jsonParser, onPost)
 
 // Please don't change this; this is needed to deploy on Heroku.
 const port = process.env.PORT || 3000;
